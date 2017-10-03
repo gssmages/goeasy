314c124a01a7552ac0b784f46213d3418e4b180f
Magesh Babu S <mageshbabu.s@flex.com>
2017-10-03 07:21:05 -0700

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_adhocrequest', app_adhocrequest);
 function app_adhocrequest($scope, app) {
     'use strict';
     app.init($scope);
+    
+     $scope.showMenu = function() {
+	     app.go('menuleft');
+	 };
 }

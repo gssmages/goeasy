7b1c6684731e987f63277e76813e7fcc92c7246a
Magesh Babu S <mageshbabu.s@flex.com>
2017-10-04 23:39:12 -0700

    [Studio] Modified screen "Home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -2,14 +2,18 @@ angular.module('app').controller('app_home', app_home);
 function app_home($scope, app) {
     'use strict';
     app.init($scope);
-    
+    $scope.pickupdiv = true;
+       $scope.dropdiv = false;
     $scope.showdiv = function(data) {
-            $scope.showA = true;
-        $scope.showB = false;
-    
-        $scope.add = function() {
-            $scope.showA = false;
-            $scope.showB = true;
-        };
+        if(data=="pickup")
+        {
+             $scope.pickupdiv = true;
+            $scope.dropdiv = false;
+        }
+        else
+        {
+             $scope.pickupdiv = false;
+            $scope.dropdiv = true;
+        }
     };
 }

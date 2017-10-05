71586c62bb7aef392185284a3566d9e801c53373
Magesh Babu S <mageshbabu.s@flex.com>
2017-10-04 23:36:18 -0700

    [Studio] Modified screen "Home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -3,5 +3,13 @@ function app_home($scope, app) {
     'use strict';
     app.init($scope);
     
+    $scope.showdiv = function(data) {
+            $scope.showA = true;
+        $scope.showB = false;
     
+        $scope.add = function() {
+            $scope.showA = false;
+            $scope.showB = true;
+        };
+    };
 }

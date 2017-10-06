dcbf0cdfe5e21ba7da74c83c770bd184da5f102c
Magesh Babu S <mageshbabu.s@flex.com>
2017-10-06 00:19:26 -0700

    [Studio] Add screen "SideMenu"

--- client/states.js
+++ client/states.js
@@ -96,5 +96,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.sidemenu', {
+        views: {
+            app: {
+                controller: 'app_sidemenu',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.sidemenu');
+                }
+            }
+        }
     });
 });

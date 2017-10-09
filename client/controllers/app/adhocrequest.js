f9fdc80913370de595a1f1b573cc0f332a111214
Magesh Babu S <mageshbabu.s@flex.com>
2017-10-08 22:59:25 -0700

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -2,7 +2,8 @@ angular.module('app').controller('app_adhocrequest', app_adhocrequest);
 function app_adhocrequest($scope, app) {
     'use strict';
     app.init($scope, function () {
-        console.log("data objects ");
+        console.log("data objects ", $scope.data);
+        
         if ($scope.data.RequestType) {
             $scope.RequestType = $scope.data.RequestType.options;
         }

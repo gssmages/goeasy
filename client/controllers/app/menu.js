f3542f132e139f0528a79879a63fd3ca7871342d
Magesh Babu S <mageshbabu.s@flex.com>
2017-10-09 04:03:09 -0700

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -1,10 +1,11 @@
 angular.module('app').controller('app_menu', app_menu);
-function app_menu($scope,$location, app) {
+function app_menu($scope,$location,$window, app) {
     'use strict';
     app.init($scope);
     
     $scope.showDashboard = function() {
 	    /*app.call('app.home');*/
-	    $location.path('http://hkdnte250:82/');
+	    $window.location.href = 'http://hkdnte250:82/';
+	    /*$location.path('http://hkdnte250:82/');
 	};
 }

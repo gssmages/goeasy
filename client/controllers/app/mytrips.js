f75169ae27ea52b79a088e2439d87551a4db24a2
selvabharathi <selvabharathi.p@flex.com>
2017-10-12 00:13:18 -0700

    [Studio] Modified screen "mytrips"

--- client/controllers/app/mytrips.js
+++ client/controllers/app/mytrips.js
@@ -2,9 +2,10 @@ angular.module('app').controller('app_mytrips', app_mytrips);
 function app_mytrips($scope, app) {
     'use strict';
     app.init($scope);
-    $scope.noShowPopup = function(){
+    $scope.noShowPopup = function(item){
         console.log("no show click...!!!");
-     app.action('mytripstab', 'mytrips[item].cancel', this)    
+     app.action('mytripstab', 'mytrips[item].cancel', this);
+     
     }
     
 }

63d28ed7aeadf38b5cb1a19b9e9e95be2a0c03b4
Magesh Babu S <mageshbabu.s@flex.com>
2017-10-12 07:39:33 -0700

    [Extension] Connect screens to page model "tripCancellation"

--- client/mocks/tripcancellation.js
+++ client/mocks/tripcancellation.js
@@ -1,3 +1,3 @@
 'use strict';
 window.mocks = window.mocks || {};
-window.mocks.tripcancellation = window.mocks.tripcancellation || {};
+window.mocks.tripCancellation = window.mocks.tripCancellation || {};

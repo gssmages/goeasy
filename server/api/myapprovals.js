614a77d1e11e20b8dc828516555e267c197ba988
gssmages <mageshbabu.s@flex.com>
2017-10-12 16:37:51 +0530

    save

---
+++ server/api/myapprovals.js
@@ -0,0 +1,13 @@
+  var logger = require('powwow-server-common').logger;
+
+exports.approve = function (page, params) {
+  
+    logger.info('Sending myapproval page data to save ', params);
+    page.update('myapprovals', params);
+    setTimeout(function () {
+        page.action('myapprovals', 'approve');
+       /* setTimeout(function () {
+             page.action('myapprovals', 'approve');
+        }, 5000);*/
+    }, 2000);
+}

5980b3b92b66c3b0f1df1ed42644a83e82a98c08
selvabharathi <selvabharathi.p@flex.com>
2017-10-13 03:48:40 -0700

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -3,7 +3,12 @@ function app_adhocrequest($scope, app) {
     'use strict';
     app.init($scope, function () {
         console.log("data objects ", $scope.data);
-        
+
+    $scope.submitApprovals = function(){
+         console.log("---sendRequest---");
+			 app.call('myapprovals.sendRequest', $scope.data);
+
+		 };
        /* if ($scope.data.RequestType) {
             $scope.RequestType = $scope.data.RequestType.options;
         }

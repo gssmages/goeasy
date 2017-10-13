2b54ec82339234a1661ee0b93194ec519365d361
gssmages <32328556+gssmages@users.noreply.github.com>
2017-10-13 17:15:16 +0530

    adhoc request

--- server/api/myapprovals.js
+++ server/api/myapprovals.js
@@ -11,3 +11,15 @@ exports.approve = function (page, params) {
         }, 5000);*/
     }, 2000);
 }
+
+exports.sendRequest = function (page, params) {
+    
+      logger.info('Sending adhoc page data to save ', params);
+      page.update('adhocrequest', params);
+      setTimeout(function () {
+          page.action('myapprovals', 'sendRequest');
+         /* setTimeout(function () {
+               page.action('myapprovals', 'approve');
+          }, 5000);*/
+      }, 2000);
+  }

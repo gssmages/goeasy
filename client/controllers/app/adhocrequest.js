2ec49cef749a8cea5117b2af8e6567166c010495
selvabharathi <selvabharathi.p@flex.com>
2017-10-16 03:59:26 -0700

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -9,21 +9,33 @@ function app_adhocrequest($scope, app) {
          //document.querySelectorAll('#row.2').hide();
     $scope.typeChange = function(){
         console.log("---Request Type---"+JSON.stringify($scope.data.RequestType.selected.label));
-        var selectedReqTyp = JSON.stringify($scope.data.RequestType.selected.label);
-        if(selectedReqTyp == "Adhoc"){
-            console.log("in if");
-             $scope.dateField = true;
-            $scope.fromDateField = false;
-            $scope.toDateField = false;
-        }else if(selectedReqTyp == "Holiday"){
-            console.log("in Else--"+typeof(selectedReqTyp));
-           $scope.dateField = true;
-            $scope.fromDateField = false;
-            $scope.toDateField = false;
-        }else{
-            $scope.dateField = false;
-            $scope.fromDateField = true;
-            $scope.toDateField = true;
+        var selectedReqTyp = parseInt(JSON.stringify($scope.data.RequestType.selected.value));
+        switch (selectedReqTyp) {
+            
+            case 2:
+                // code
+                console.log("in adhoc--"+selectedReqTyp);
+                $scope.dateField = true;
+                $scope.fromDateField = false;
+                $scope.toDateField = false;
+                break;
+            case 3:
+                // code
+                console.log("in holiday--"+selectedReqTyp);
+                $scope.dateField = true;
+                $scope.fromDateField = false;
+                $scope.toDateField = false;
+                break;
+            case 4:
+                // code
+                console.log("in month--"+selectedReqTyp);
+                $scope.dateField = false;
+                $scope.fromDateField = true;
+                $scope.toDateField = true;
+                break;    
+            default:
+            console.log("in default--"+selectedReqTyp);
+                // code
         }
     }    
     $scope.submitApprovals = function(){

ba78541ec7c304ff311bd97c5ce48174ba32d812
selvabharathi <selvabharathi.p@flex.com>
2017-10-25 03:05:44 -0700

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -42,7 +42,7 @@ function app_adhocrequest($scope, app) {
         
     }    
     $scope.specialNeedModified = function(){
-        console.log("---specialNeedModified---"+$scope.data.SpecialNeed);
+        console.log("---specialNeedModified---"+JSON.stringify($scope.data.SpecialNeed));
     };
     $scope.submitApprovals = function(){
          //console.log("---sendRequest---"+$scope.data.Date);

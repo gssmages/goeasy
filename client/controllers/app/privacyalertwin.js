0586084b06f7984224a9822888f8067eed8d376c
Magesh Babu S <mageshbabu.s@flex.com>
2017-11-03 03:18:12 -0700

    [Studio] Modified screen "privacyAlertWin"

--- client/controllers/app/privacyalertwin.js
+++ client/controllers/app/privacyalertwin.js
@@ -2,8 +2,6 @@ angular.module('app').controller('app_privacyalertwin', app_privacyalertwin);
 function app_privacyalertwin($scope, app,$sce) {
     'use strict';
     app.init($scope);
-    $scope.$sce = $sce;
-    
-    $scope.privacyinfo = $sce.trustAsHtml($scope.data.privacyInfo);
+    $scope.privacycontent = $sce.trustAsHtml($scope.data.privacyInfo);
     
 }
